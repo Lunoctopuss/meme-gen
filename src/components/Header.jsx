2bc3988cb52fefbd06a199cf00e1233f76dbd226
Mona Lisa <krojek7@gmail.com>
2022-01-20 17:30:04 +0000

    commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import '../App.css';
+
+function Header() {
+    return (
+        <header className='titleHeader'>
+                < img src="https://www.pngall.com/wp-content/uploads/7/Cute-Emoticon-PNG-Download-Image.png" 
+                alt='Smile face'
+                />
+            <h1>Meme Girls Presents:</h1>
+            <h1 className="MemeTitle">Meme Generator</h1>
+        </header>
+    )
+}
+
+export default Header
